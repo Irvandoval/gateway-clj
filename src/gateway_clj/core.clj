f439c18d9a306a307b8156852a86d384351a1651
Irvin Sandoval <irvandoval@.com>
2019-06-13 07:40:59 -0600

    jetty

--- src/gateway_clj/core.clj
+++ src/gateway_clj/core.clj
@@ -1,8 +1,10 @@
 (ns gateway-clj.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :refer [site]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.adapter.jetty :as jetty]
             [gateway-clj.handlers.messaging :refer [inbound-handler]]))
 
 (defroutes app-routes
@@ -14,3 +16,7 @@
              (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
              wrap-json-response
              (wrap-json-body {:keywords? true :bigdecimals? true})))
+
+(defn -main [& [port]]
+  (let [port (Integer. 5000)]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
