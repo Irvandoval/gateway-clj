edebb2d72277a00428db2520d71b97cf1dc1f72a
Irvin Sandoval <irvandoval@.com>
2019-06-13 08:11:23 -0600

    show request

--- src/gateway_clj/handlers/messaging.clj
+++ src/gateway_clj/handlers/messaging.clj
@@ -5,6 +5,6 @@
 (def messages-endpoint "/v1.1/apps/{appId}/appusers/{userId}/messages")
 
 (defn inbound-handler [request]
-  (println (str "Message received " request))
+  (println (str "Message received " (get-in request [:body])))
   (response {:message "Message Sent"}))
 
